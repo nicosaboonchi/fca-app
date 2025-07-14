e61c843b7d5875ff4d93d52affcf9c8bc33e9b17
nicosaboonchi <nico.saboonchi@gmail.com>
2025-07-14 16:11:31 -0700

    add Button component and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the FCA App</h1>
       <p>This is the home page of your Next.js application.</p>
+      <Button>Click Me</Button>
     </div>
   );
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-lg font-medium hover:cursor-pointer",
+  {
+    variants: {
+      variant: {
+        default: "bg-gray-500 text-white hover:bg-gray-600",
+      },
+      size: {
+        md: "px-4 py-2",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "md",
+    },
+  }
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? "span" : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size }), className)}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button };
