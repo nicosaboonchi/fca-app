ec546fcc8d71194451c5d2fcf8467f0060fad757
Nico Saboonchi <130340602+nicosaboonchi@users.noreply.github.com>
2025-09-17 11:44:11 -0700

    Adjust sidebar toggle and collapse behavior

--- src/app/(group)/layout.tsx
+++ src/app/(group)/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from "../components/ui/sidebar";
 import { AppSidebar } from "../components/app-sidebar";
 import ModeToggle from "../components/mode-toggle";
